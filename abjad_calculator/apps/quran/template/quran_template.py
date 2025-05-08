a847d62486d964682054e897c214ea1a5e3104e6
Mohammad Abuzar Hashemi <mohammadabuzarhashemi@Mohammads-MacBook-Pro.local>
2025-05-08 03:08:30 -0700

    fix margin in surahs

--- abjad_calculator/apps/quran/template/quran_template.py
+++ abjad_calculator/apps/quran/template/quran_template.py
@@ -336,7 +336,7 @@ quran_html_template_start = """
             position: relative;
             /*padding-top: 30px;*/
             margin: 8px auto;
-            background-color: #fff;
+            /*background-color: #fff;*/
             box-shadow: 0 5px 20px rgba(0,0,0,0.2);
             border: 2px solid var(--border-color);
         }
@@ -397,7 +397,7 @@ quran_html_template_start = """
             .surah_header {
                 position: relative;
                 /* padding-top: 20px;*/
-                margin-bottom: 8px;
+                margin: 8px auto;
                 box-shadow: 0 5px 20px rgba(0,0,0,0.2);
                 border: 2px solid var(--border-color);
             }
