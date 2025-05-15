68bf61c2798a03044f0d20eda788c614d6673921
Mohammad Abuzar Hashemi <mohammadabuzarhashemi@Mohammads-MacBook-Pro.local>
2025-05-15 00:20:41 -0700

    surah 97, 98 corrections

--- abjad_calculator/quran_library/al_qadr.py
+++ abjad_calculator/quran_library/al_qadr.py
@@ -1,6 +1,6 @@
 ayats = [
     {
-        "arabic_text": "اِنَّا اَنزَلْنَاهُ فِي لَيْلَةِ الْقَدْرِ",
+        "arabic_text": "اِنَّآ اَنزَلْنٰهُ فِي لَيْلَةِ الْقَدْرِ",
         "urdu_text": "بے شک ہم نے اسے شب قدر میں نازل کیا۔",
         "persian_text": "ما آن را در شب قدر نازل کردیم.",
         "english_text": "Indeed, We sent it down during the Night of Decree.",
@@ -8,7 +8,7 @@ ayats = [
         "verse_number": "1"
     },
     {
-        "arabic_text": "وَمَآ اَدْرَىٰكَ مَا لَيْلَةُ الْقَدْرِ",
+        "arabic_text": "وَ مَآ اَدْرٰكَ مَا لَيْلَةُ الْقَدْرِ",
         "urdu_text": "اور آپ کو کیا معلوم کہ شب قدر کیا ہے؟",
         "persian_text": "و چه می‌دانی شب قدر چیست؟",
         "english_text": "And what can make you know what is the Night of Decree?",
@@ -16,7 +16,7 @@ ayats = [
         "verse_number": "2"
     },
     {
-        "arabic_text": "لَيْلَةُ الْقَدْرِ خَيْرٌ مِّنْ اَلْفِ شَهْرٍۢ",
+        "arabic_text": "لَيْلَةُ الْقَدْرِ خَيْرٌ مِّنْ اَلْفِ شَهْرٍ",
         "urdu_text": "شب قدر ہزار مہینوں سے بہتر ہے۔",
         "persian_text": "شب قدر بهتر از هزار ماه است.",
         "english_text": "The Night of Decree is better than a thousand months.",
@@ -24,7 +24,7 @@ ayats = [
         "verse_number": "3"
     },
     {
-        "arabic_text": "تَنَزَّلُ الْمَلَـٰٓئِكَةُ وَالرُّوحُ فِيهَا بِاِذْنِ رَبِّهِم مِّن كُلِّ اَمْرٍۢ",
+        "arabic_text": "تَنَزَّلُ الْمَلٰٓئِكَةُ وَ الرُّوحُ فِيهَا بِاِذْنِ رَبِّهِم مِّن كُلِّ اَمْرٍ",
         "urdu_text": "اس میں فرشتے اور روح القدس اپنے رب کے حکم سے ہر کام کے لیے اترتے ہیں۔",
         "persian_text": "فرشتگان و روح در آن به فرمان پروردگارشان برای هر امر نازل می‌شوند.",
         "english_text": "The angels and the Spirit descend therein by permission of their Lord for every matter.",
@@ -32,7 +32,7 @@ ayats = [
         "verse_number": "4"
     },
     {
-        "arabic_text": "سَلَـٰمٌ هِىَ حَتَّىٰ مَطْلَعِ الْفَجْرِ",
+        "arabic_text": "سَلٰمٌ هِىَ حَتّٰى مَطْلَعِ الْفَجْرِ",
         "urdu_text": "وہ سلامتی کی رات ہے جو فجر کے طلوع ہونے تک رہتی ہے۔",
         "persian_text": "آن شب سلامتی است تا طلوع فجر.",
         "english_text": "Peace it is until the emergence of dawn.",
